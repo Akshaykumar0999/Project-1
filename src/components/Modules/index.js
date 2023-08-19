cfd584e78e3c7e919b43066866edee9b687cf808
Akshay <kanikeakshay55@gmail.com>
2023-08-19 14:12:13 +0530

    changes added

--- src/components/Modules/index.js
+++ src/components/Modules/index.js
@@ -1,4 +1,5 @@
-import { Component } from 'react'
+import React, { useState } from 'react'
+
 import ModuleData from '../ModuleData'
 
 import {AiFillHome, AiFillSetting, AiFillQuestionCircle} from 'react-icons/ai'
@@ -11,42 +12,42 @@ import './index.css'
 
 const VariousModulesDetailsList = [
     {
-        id: 0,
+        id: 1,
         IconComponent: AiFillHome,
         IconName: "Dashboard",
         linkTo: '/',
         colorName: "#0d99b8"
     },
     {
-        id: 1,
+        id: 2,
         IconComponent: FaHandshake,
         IconName: "Service Seekers",
         linkTo: '/',
         colorName: "#0c0d4a"
     },
     {
-        id: 2,
+        id: 3,
         IconComponent: RiLuggageCartFill,
         IconName: "Service Providers",
         linkTo: '/',
         colorName: "#e1c62e"
     },
     {
-        id: 3,
+        id: 4,
         IconComponent: MdOutlineSpeakerNotes,
         IconName: "Jobs",
         linkTo: '/dashboard/category',
         colorName: "#3ab13e"
     },
     {
-        id: 4,
+        id: 5,
         IconComponent: FaTools,
         IconName: "Categories",
         linkTo: '/',
         colorName: "#93e8f7"
     },
     {
-        id: 5,
+        id: 6,
         IconComponent: FaFlag,
         IconName: "Sub Categories",
         linkTo: '/',
@@ -54,49 +55,49 @@ const VariousModulesDetailsList = [
     },
 
     {
-        id: 6,
+        id: 7,
         IconComponent: MdOutlineSpeakerNotes,
         IconName: "Earning",
         linkTo: '/dashboard/category/subcategories',
         colorName: "#4ebdd6"
     },
     {
-        id: 7,
+        id: 8,
         IconComponent: MdGroups2,
         IconName: "Repots",
         linkTo: '/',
         colorName: "#e1c62e"
     },
     {
-        id: 8,
+        id: 9,
         IconComponent: FaPuzzlePiece,
         IconName: "Tikects",
         linkTo: '/',
         colorName: "#0c0d4a"
     },
     {
-        id: 9,
+        id: 10,
         IconComponent: AiFillSetting,
         IconName: "Notifications",
         linkTo: '/',
         colorName: "#0d99b8"
     },
     {
-        id: 10,
+        id: 11,
         IconComponent: BsPersonFill,
         IconName: "Privacy And Polacy",
         linkTo: '/',
         colorName: "#ed8489"
     },
     {
-        id: 11,
+        id: 12,
         IconComponent: BsFillBellFill,
         IconName: "FAQ's",
         linkTo: '/',
         colorName: "#dfee68"
     },
     {
-        id: 12,
+        id: 13,
         IconComponent: AiFillQuestionCircle,
         IconName: "Settings",
         linkTo: '/',
@@ -105,24 +106,19 @@ const VariousModulesDetailsList = [
 
 ]
 
-class Modules extends Component {
-    state = {actId : VariousModulesDetailsList[0].id}
-
-    updateActId = (id) => {
-        this.setState({actId: id})
+const Modules = () => {
+    const [ActiveId, setModulesListActId] = useState([]);
+    const updateActId = (id) => {
+        setModulesListActId(id)
     }
-
-    render(){
-        const {actId} = this.state
-        return(
-            <div className='Dash-board-lists-container'>
-                <MdOutlineWbTwilight className='logo-icon-is' />
-                <ul className='app-dash-board-lists'>
-                    {VariousModulesDetailsList.map(eachItem => (<ModuleData key={eachItem.id} details={eachItem} updateActId={this.updateActId} isActive={actId === eachItem.id} />))}
-                </ul>
-            </div>
-        )
-    }
+    return(
+        <div className='Dash-board-lists-container'>
+            <MdOutlineWbTwilight className='logo-icon-is' />
+            <ul className='app-dash-board-lists'>
+                {VariousModulesDetailsList.map(eachItem => (<ModuleData key={eachItem.id} details={eachItem} updateActId={updateActId} isActive={ActiveId === eachItem.id} />))}
+            </ul>
+        </div>
+    )
 }
 
 export default Modules
